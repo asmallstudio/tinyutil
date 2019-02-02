b4ce55a2918219ab1b8a8429e32ef12aa952caae
Scott Smith <mail@ScottHSmith.com>
2019-02-01 18:51:19 -0800

    add components

---
+++ packages/components/ambiLink/AmbiLink.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "@reach/router";
+
+class AmbiLink extends React.Component {
+  isInternal(to) {
+    if (typeof to !== "string") return true;
+    return (
+      !to.match(/^#/) &&
+      !to.match(/^[a-z]{1,10}:\/\//) &&
+      !to.match(/^mailto:|^tel:/)
+    );
+  }
+
+  render() {
+    const { to = "", children, ...rest } = this.props;
+    const isInternal = this.isInternal(to);
+    if (isInternal) {
+      return (
+        <Link to={to} {...rest}>
+          {children}
+        </Link>
+      );
+    }
+    return (
+      <a href={to} {...rest}>
+        {children}
+      </a>
+    );
+  }
+}
+
+export default AmbiLink;
---
+++ packages/components/reactMarkdownLink/ReactMarkdownLink.js
@@ -0,0 +1,8 @@
+import React from "react";
+import AmbiLink from "../ambiLink/AmbiLink";
+
+const ReactMarkdownLink = props => (
+  <AmbiLink to={props.href}>{props.children}</AmbiLink>
+);
+
+export default ReactMarkdownLink;
