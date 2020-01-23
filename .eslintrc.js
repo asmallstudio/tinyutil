542af737cb75cf9401bad9d9231d9682c0a901c1
Scott Smith <mail@ScottHSmith.com>
2020-01-23 14:00:05 -0800

    fix undefined React

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
   ],
   rules: {
     "react/prop-types": "off",
+    "react/react-in-jsx-scope": "error",
     "block-scoped-var": "warn",
     "guard-for-in": "error",
     "new-cap": "warn",
